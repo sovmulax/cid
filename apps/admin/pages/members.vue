424da0684bd71fd855b3b1b5d6eb2077a09e23c8
sovmulax <soumailaevane@gmail.com>
2023-04-03 19:54:04 +0000

    member listing

--- apps/admin/pages/members.vue
+++ apps/admin/pages/members.vue
@@ -1,11 +1,46 @@
-<script lang="ts" setup></script>
+<script setup>
+import { ref } from 'vue';
+import pb from '../pocket.config.js';
+
+// collect data from pocketbase
+let records;
+try {
+  records = await pb.collection('members').getFullList();
+} catch (error) {}
+const data = ref(records);
+</script>
 
 <template>
   <NuxtLayout name="body">
-    <template #title-header>
-      <h5>Membres</h5>
+    <template #title-header> Membres </template>
+    <template #content>
+      <div class="row">
+        <div v-for="item in data" :key="item.id" class="col-md-6 col-xl-3">
+          <div class="card">
+            <img
+              class="card-img-top img-fluid size"
+              :src="`https://pb.marcaureln.com/api/files/` + item.collectionId + `/` + item.id + `/` + item.picture"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title mt-0">{{ item.fullname }}</h5>
+              <h6 class="card-title mt-0">{{ item.jobTitle }}</h6>
+              <p class="card-text">
+              <a :href="item.email"><i class="mdi mdi-email font-20"></i> {{ item.email }}</a><br>
+              <a :href="item.linkedin"><i class="mdi mdi-linkedin-box font-20"></i> {{ item.linkedin }}</a><br>
+              <a :href="item.facebook"><i class="mdi mdi-facebook-box font-20"></i> {{ item.facebook }}</a><br>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </template>
   </NuxtLayout>
 </template>
 
-<style scoped></style>
+<style scoped>
+.size {
+  height: 200px;
+  object-fit: cover;
+}
+</style>
