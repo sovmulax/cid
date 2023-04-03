bc1c98b076da6c1da599aa5b12b08470098b1cd6
sovmulax <soumailaevane@gmail.com>
2023-04-03 19:44:00 +0000

    listing post

--- apps/admin/pages/post.vue
+++ apps/admin/pages/post.vue
@@ -1,9 +1,55 @@
-<script lang="ts" setup></script>
+<script setup>
+import { ref } from 'vue';
+import pb from '../pocket.config.js';
+
+// collect data from pocketbase
+let records;
+try {
+  records = await pb.collection('post').getFullList();
+} catch (error) {}
+const data = ref(records);
+const i = 1;
+
+async function author(id) {
+  const author = await pb.collection('users').getOne(id);
+  return author.name;
+}
+</script>
 
 <template>
   <NuxtLayout name="body">
-    <template #title-header>
-      <h5>Actualités</h5>
+    <template #title-header> Actualités </template>
+    <template #content>
+      <div class="row">
+        <div class="col-md-12 card">
+          <div class="card-body">
+            <h4 class="mt-0 header-title">Liste des Articles</h4>
+            <p class="text-muted mb-4"></p>
+            <div class="table-responsive">
+              <table class="table table-striped mb-0">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Titre</th>
+                    <th>Date</th>
+                    <th>Détails</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="item in data" :key="item.id">
+                    <th scope="row">{{ i++ }}</th>
+                    <td>{{ item.title }}</td>
+                    <td>
+                      {{ item.created }}
+                    </td>
+                    <td><nuxt-link to="/post">...</nuxt-link></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </template>
   </NuxtLayout>
 </template>
