15ba2bf5c39c237f4e413fe09a3eb5eee9af7045
konan-fabrice <kouameaimefabrice@gmail.com>
2023-05-09 19:41:33 +0000

    init

---
+++ apps/admin/pages/ajoutevent.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <NuxtLayout name="body">
+    <template #title-header>
+      <h5>Projets</h5>
+    </template>
+  </NuxtLayout>
+</template>
+
+<style scoped></style>
