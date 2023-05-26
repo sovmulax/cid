87f95f868644ff61c8ba1e94536922b3510e496d
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-05-26 13:13:34 +0000

    Make form field required

--- apps/web/components/ContactFormBusiness.vue
+++ apps/web/components/ContactFormBusiness.vue
@@ -4,24 +4,28 @@
       class="focus:shadow-outline appearance-none rounded border p-3 leading-tight text-gray-700 shadow focus:outline-none"
       type="text"
       placeholder="Nom de l'entreprise"
+      required
     />
 
     <input
       class="focus:shadow-outline appearance-none rounded border p-3 leading-tight text-gray-700 shadow focus:outline-none"
       type="text"
       placeholder="Email/Numéro de téléphone"
+      required
     />
 
     <input
       class="focus:shadow-outline appearance-none rounded border p-3 leading-tight text-gray-700 shadow focus:outline-none"
       type="text"
       placeholder="Objet"
+      required
     />
 
     <textarea
       class="focus:shadow-outline appearance-none rounded border p-3 leading-tight text-gray-700 shadow focus:outline-none"
       rows="4"
       placeholder="Parlez-nous de votre projet"
+      required
     ></textarea>
 
     <button class="mt-3 rounded-md bg-primary p-3 text-xl font-bold text-white" type="submit">Envoyer</button>
