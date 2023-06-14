9554e83fa7482f7478a1d38619fd2d4ec59d610a
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-06-14 15:29:53 +0000

    Add responsiveness

--- apps/web/components/AboutGallery.vue
+++ apps/web/components/AboutGallery.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="flex flex-col items-center gap-12 pb-24 pt-12 lg:px-64">
+  <section class="flex flex-col items-center gap-12 px-9 pb-24 pt-12 lg:px-64">
     <h1 class="text-6xl font-bold tracking-tight">Galerie</h1>
 
     <div class="container">
       <div class="-m-1 flex flex-wrap md:-m-2">
-        <div v-for="index in 6" :key="index" class="flex w-1/3 flex-wrap">
+        <div v-for="index in 6" :key="index" class="flex flex-wrap md:w-1/2 lg:w-1/3">
           <div class="w-full p-1 md:p-2">
             <img
               alt="gallery"
--- apps/web/components/HomeContact.vue
+++ apps/web/components/HomeContact.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="flex flex-col items-center gap-9 bg-primary px-9 py-16 text-white md:px-96">
-    <h1 class="text-6xl font-bold tracking-tight">Vous avez un projet ?</h1>
+  <section class="flex flex-col items-center gap-9 bg-primary px-9 py-16 text-white lg:px-96">
+    <h1 class="text-center text-6xl font-bold tracking-tight">Vous avez un projet ?</h1>
     <p class="text-center">
       Laissez-nous vous aider à lui donner vie ! Notre équipe de professionnels qualifiés possède l'expertise et
       l'expérience nécessaires pour transformer vos idées en un site web époustouflant qui attirera et engagera votre
