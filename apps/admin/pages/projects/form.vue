7cc66394c22bbfb125815fc995234df6ad99c687
sovmulax <soumailaevane@gmail.com>
2023-06-29 12:21:49 +0000

    form befor logic

--- apps/admin/pages/projects/form.vue
+++ apps/admin/pages/projects/form.vue
@@ -1,5 +1,16 @@
 <script setup>
+import { ref } from 'vue';
 import pb from '../../pocket.config.js';
+
+const intitule = ref('');
+const domaine = ref('');
+const subdomaine = ref('');
+const start = ref('');
+const end = ref('');
+const member0 = ref('');
+const role0 = ref('');
+const task0 = ref('');
+
 // collect data from pocketbase
 try {
   records = await pb.collection('projects').getFullList();
@@ -13,10 +24,13 @@ function ajouterLigne() {
   const doc = document.getElementById('inputContainer');
 
   const row1 = document.createElement('div');
-  row1.className = 'col-6';
+  row1.className = 'col-4';
 
   const row2 = document.createElement('div');
-  row2.className = 'col-6';
+  row2.className = 'col-4';
+
+  const row3 = document.createElement('div');
+  row3.className = 'col-4';
 
   const input = document.createElement('input');
   input.type = 'text';
@@ -28,10 +42,17 @@ function ajouterLigne() {
   input0.name = 'task' + i.toString();
   input0.className = 'form-control mb-2';
 
+  const input1 = document.createElement('input');
+  input1.type = 'text';
+  input1.name = 'role' + i.toString();
+  input1.className = 'form-control mb-2';
+
   doc.appendChild(row1);
   doc.appendChild(row2);
+  doc.appendChild(row3);
   row1.appendChild(input);
   row2.appendChild(input0);
+  row3.appendChild(input1);
 }
 </script>
 
@@ -47,48 +68,62 @@ function ajouterLigne() {
                 <div class="form-group">
                   <label>Intitulé du Projet</label>
                   <div>
-                    <input type="text" class="form-control" placeholder="Min 6 chars." />
+                    <input v-model="intitule" type="text" class="form-control" />
                   </div>
                 </div>
                 <div class="form-group">
                   <label>Domaine</label>
                   <div>
-                    <select name="" class="form-control">
-                      <option value="" selected>Selectionné une valeur</option>
-                      <option value="">test</option>
-                      <option value="">test</option>
-                    </select>
+                    <input v-model="domaine" type="text" class="form-control" />
                   </div>
                 </div>
                 <div class="form-group">
-                  <label>Sous Domaine</label>
+                  <label>Sous-Domaine</label>
                   <div>
-                    <select name="" class="form-control">
-                      <option value="" selected>Selectionné une valeur</option>
-                      <option value="">test</option>
-                      <option value="">test</option>
-                    </select>
+                    <input v-model="subdomaine" type="text" class="form-control" />
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="row">
+                    <div class="col-6 form-group">
+                      <label>Date de Début</label>
+                      <input v-model="start" type="date" class="form-control" />
+                    </div>
+                    <div class="col-6 form-group">
+                      <label>Date de Fin</label>
+                      <input v-model="end" type="date" class="form-control" />
+                    </div>
                   </div>
                 </div>
                 <div class="row">
                   <div class="col-12">
                     <h5>Liste des Membres</h5>
                     <div id="inputContainer" class="row">
-                      <div class="col-6">
+                      <div class="col-4">
                         <label for="tach">Membre</label>
                         <input
+                          v-model="member0"
                           type="text"
-                          name="member0"
                           class="form-control mb-2"
                           required
                           placeholder="Nom de l'exécutant"
                         />
                       </div>
-                      <div class="col-6">
+                      <div class="col-4">
+                        <label for="tach">Role</label>
+                        <input
+                          v-model="role0"
+                          type="text"
+                          class="form-control mb-2"
+                          required
+                          placeholder="Intitulé de la tâche"
+                        />
+                      </div>
+                      <div class="col-4">
                         <label for="tach">Tache</label>
                         <input
+                          v-model="task0"
                           type="text"
-                          name="task0"
                           class="form-control mb-2"
                           required
                           placeholder="Intitulé de la tâche"
