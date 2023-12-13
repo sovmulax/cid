195d9e109d0c48c3037759530fec659bf7fbbfd1
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-12-13 14:15:51 +0000

    Refactor dateformat function and handle empty data

--- utils/date.ts
+++ utils/date.ts
@@ -1,8 +1,6 @@
-export function dateformat(data: any) {
-  // use timestamp to format date
-  const date = new Date(data);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  return `${day}/${month}/${year}`;
+export function dateformat(value: any) {
+  if (!value) return '-';
+
+  const date = new Date(value);
+  return date.toLocaleDateString();
 }
