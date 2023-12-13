e4eb4d6463b2defadc76e40a4fafa0890dd3cb30
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-12-13 14:08:30 +0000

    Perstit user state

--- composables/states.ts
+++ composables/states.ts
@@ -1,3 +1,23 @@
 import type { RecordAuthResponse } from 'pocketbase';
 
-export const useUserState = () => useState<RecordAuthResponse | null>('user', () => null);
+// Inspired by https://github.com/nuxt/nuxt/discussions/16067#discussioncomment-2267207
+export const usePersistedState = <T>(identifier: string, defaultOptions: T): Ref<T> => {
+  const persistedObject = useState<T>(identifier, (): T => {
+    const item = localStorage.getItem(identifier);
+    if (!item) return defaultOptions;
+
+    return (JSON.parse(item) as T) ?? defaultOptions;
+  });
+
+  watch(
+    persistedObject,
+    (object) => {
+      localStorage.setItem(identifier, JSON.stringify(object));
+    },
+    { deep: true }
+  );
+
+  return persistedObject;
+};
+
+export const useUserState = () => usePersistedState<RecordAuthResponse | null>('user', null);
