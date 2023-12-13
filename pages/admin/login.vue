ae1e95ad9d587bc628d25ca105cdc83b365eab47
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-12-13 16:03:48 +0000

    Refactor login page layout and error handling

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -12,13 +12,15 @@ async function login() {
     alert('Veuillez remplir tous les champs');
   }
 
-  const response = await $pb.collection('users').authWithPassword(email.value, password.value);
+  try {
+    const response = await $pb.collection('users').authWithPassword(email.value, password.value);
 
-  if (response) {
-    user.value = response;
-    $router.push('/admin/projects');
-  } else {
-    alert('Email ou mot de passe incorrect');
+    if (response) {
+      user.value = response;
+      $router.push('/admin/projects');
+    }
+  } catch (error) {
+    alert(error);
   }
 
   pending.value = false;
@@ -30,56 +32,58 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="account-pages my-5 pt-5">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8 col-lg-6 col-xl-5">
-          <div class="card bg-pattern shadow-none">
-            <div class="card-body">
-              <form class="p-3" @submit.prevent="login">
-                <h4 class="font-18 text-center">Connexion</h4>
-                <br />
-                <div class="form-horizontal">
-                  <div class="form-group">
-                    <label for="username">Email</label>
-                    <input
-                      id="username"
-                      v-model="email"
-                      type="text"
-                      class="form-control"
-                      placeholder="Email"
-                      required
-                    />
-                  </div>
+  <div class="account-pages">
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+        <a href="/" class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
+          <img class="w-12 object-contain" src="~/assets/img/logo.png" alt="Logo CID" />
+        </a>
+        <div
+          class="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0"
+        >
+          <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+            <h1 class="text-center text-xl font-bold text-gray-900 dark:text-white md:text-2xl">
+              Espace administrateur
+            </h1>
+            <form class="space-y-4 md:space-y-6" @submit.prevent="login">
+              <div>
+                <label for="email" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"> Email </label>
+                <input
+                  id="email"
+                  v-model="email"
+                  type="text"
+                  class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+                  placeholder="admin@acme.co"
+                  required
+                />
+              </div>
+              <div>
+                <label for="password" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                  Mot de passe
+                </label>
+                <input
+                  id="password"
+                  v-model="password"
+                  type="password"
+                  name="password"
+                  placeholder="••••••••"
+                  class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+                  required
+                />
+              </div>
 
-                  <div class="form-group">
-                    <label for="password">Mot de Passe</label>
-                    <input
-                      id="password"
-                      v-model="password"
-                      type="password"
-                      class="form-control"
-                      placeholder="Mot de passe"
-                      required
-                    />
-                  </div>
-
-                  <div class="mt-3">
-                    <button
-                      class="btn btn-primary btn-block waves-effect waves-light"
-                      :class="{ 'cursor-not-allowed': pending }"
-                      type="submit"
-                      :disabled="pending"
-                    >
-                      Se connecter
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
+              <button
+                class="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                :class="{ 'cursor-not-allowed': pending }"
+                type="submit"
+                :disabled="pending"
+              >
+                Se connecter
+              </button>
+            </form>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
